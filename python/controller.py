cd6f0d2e4687a0813869b7d3800ee5730c667cfb
giovannifranzese94 <giovanni.franzese94@gmail.com>
2023-08-18 22:36:11 +0200

    we are now able to lunch the controller from python directly

--- python/controller.py
+++ python/controller.py
@@ -19,12 +19,22 @@ class LaunchManager:
 
         # Load the specified package and launch file
         launch_file_path = roslaunch.rlutil.resolve_launch_arguments([self.package, self.launch_file])
+
+        launch_args = 'robot_ip:= ' + self.robot_ip  # Replace 'arg_name' and 'arg_value' with your argument name and value
+
+        launch_file=[(launch_file_path[0], [launch_args])]
         self.launch_api.parent = roslaunch.parent.ROSLaunchParent(
-            rospy.get_param("/run_id"), launch_file_path
+            rospy.get_param("/run_id"), launch_file
         )
 
         # Set the robot IP as a parameter for the launch file
-        self.launch_api.parent.start([f'robot_ip:={self.robot_ip}'])
+        self.launch_api.parent.start()
+
+        ###
+
+        # parent = roslaunch.parent.ROSLaunchParent(uuid, roslaunch_file)
+
+        # parent.start()
 
     def stop(self):
         if self.launch_api is not None:
