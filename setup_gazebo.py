e04febeeffcc2af4ad36b23183adae1cc0458333
giovannifranzese94 <giovanni.franzese94@gmail.com>
2024-05-14 12:51:13 +0200

    changed the setup file that now uses rospackage to look for the right files

--- setup_gazebo.py
+++ setup_gazebo.py
@@ -1,6 +1,4 @@
-import os
-# import sys
-# input_string = sys.argv[1]
+import rospkg
 def add_text(file_path, text_to_add):
     with open(file_path, 'r+') as file:
         content = file.read()
@@ -30,18 +28,12 @@ def replace_line(file_path, search_line, replace_line):
                 file.write(replace_line + '\n')
             else:
                 file.write(line)
-# input_string = sys.argv[1]
 
-# MAIN
-# MODIFY THE YAML FILE
-# Get the current directory of the Python file
-current_dir = os.path.dirname(os.path.abspath(__file__))
-# Go one directory up
-parent_dir = os.path.dirname(current_dir)
-# Access the directory of a subdirectory
-subdirectory = os.path.join(parent_dir, 'franka_gazebo')
-# Print the path to the subdirectory
+# Print the path to the franka_gazebo
 
+ros_pack = rospkg.RosPack()
+franka_gazebo = ros_pack.get_path('franka_gazebo')
+franka_hfc = ros_pack.get_path('franka_human_friendly_controllers')
 
 new_text_cartesian=''' 
 cartesian_variable_impedance_controller:
@@ -73,14 +65,14 @@ joint_variable_impedance_controller:
 
 # # Perform the replacement
 # new_text = new_text.replace("franka_advanced_controllers", input_string)
-file_path=os.path.join(subdirectory, 'config/sim_controllers.yaml')
+file_path=franka_gazebo + '/config/sim_controllers.yaml'
 print("Change files")
 print(file_path)
 add_text(file_path, new_text_cartesian)
 add_text(file_path, new_text_joint)
 
 # MODIFY THE PACKAGE
-file_path = os.path.join(subdirectory, 'package.xml')
+file_path = franka_gazebo + '/package.xml'
 print("Change files")
 print(file_path)
 existing_depend = '<depend>franka_example_controllers</depend>'
@@ -90,7 +82,7 @@ new_depend = '  <depend>franka_human_friendly_controllers</depend>'
 search_and_paste(file_path, existing_depend, new_depend)
 
 # MODIFY CMAKE
-file_path = os.path.join(subdirectory, 'CMakeLists.txt')
+file_path = franka_gazebo + '/CMakeLists.txt'
 print("Change files in directory")
 print(file_path)
 existing_depend = 'franka_example_controllers'
@@ -100,7 +92,7 @@ new_depend = '  franka_human_friendly_controllers'
 search_and_paste(file_path, existing_depend, new_depend)
 
 # Modify the controller to not have the joint repulsion that generated obscillations in sumulation 
-file_path = parent_dir + '/franka_human_friendly_controllers/src/cartesian_variable_impedance_controller.cpp'
+file_path = franka_hfc +  '/src/cartesian_variable_impedance_controller.cpp'
 print("Change files in directory")
 print(file_path)
 search_line= 'tau_d << tau_task + tau_nullspace + coriolis+ tau_joint_limit;'
@@ -108,7 +100,7 @@ new_line='tau_d << tau_task + tau_nullspace + coriolis;'
 replace_line(file_path, search_line, new_line)
 
 # Modify the joint impedance control also
-file_path = parent_dir + '/franka_human_friendly_controllers/src/joint_variable_impedance_controller.cpp'
+file_path = franka_hfc + '/src/joint_variable_impedance_controller.cpp'
 print("Change files in directory")
 print(file_path)
 search_line= 'tau_d << tau_joint + coriolis + tau_joint_limit;'
@@ -117,14 +109,14 @@ replace_line(file_path, search_line, new_line)
 
 
 # MODIFY THE CLOCK
-file_path = os.path.join(subdirectory, 'launch/robot.launch')
+file_path = franka_gazebo +  '/launch/robot.launch'
 search_line = '<arg name="use_sim_time" value="true"/>'
 new_line = '    <arg name="use_sim_time" value="false"/>'
 print("Change files in directory")
 print(file_path)
 replace_line(file_path, search_line, new_line)
 # add the rqt in the launch file
-file_path = os.path.join(subdirectory, 'launch/panda.launch')
+file_path = franka_gazebo + '/launch/panda.launch'
 print("Change files")
 print(file_path)
 existing_depend='</include>'
